2f64b4d443531008eaba6ffafc39fa43a8cddae4
AlexAlibrandi <alexander_alibrandi@hcl.com>
2022-02-08 10:42:22 -0500

    Initial project add

---
+++ src/main/java/com/spring5/animalmagnetismpetclinic/AnimalmagnetismPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.spring5.animalmagnetismpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalmagnetismPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimalmagnetismPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring5/animalmagnetismpetclinic/AnimalmagnetismPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring5.animalmagnetismpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalmagnetismPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
