24f10d2f1c85ac825f115bb9dbd40f9310bc20b5
AlexAlibrandi <alexander_alibrandi@hcl.com>
2022-02-14 10:36:37 -0500

    Slight changes on owner controller

--- animal-magnetism-web/src/main/java/com/spring5/animalmagnetismpetclinic/controllers/VetController.java
+++ animal-magnetism-web/src/main/java/com/spring5/animalmagnetismpetclinic/controllers/VetController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class VetController {
 
-    @RequestMapping({"vets/index.html","vets","vets/index"})
+    @RequestMapping({"","vets/index.html","vets","vets/index"})
     public String listVets(){
 
         return "vets/vetIndex";
