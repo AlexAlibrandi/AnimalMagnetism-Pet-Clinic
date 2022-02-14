4ca16627c27045166accc7fd7de36c6c4551cfd6
AlexAlibrandi <alexander_alibrandi@hcl.com>
2022-02-14 14:41:33 -0500

    list all owners on owners index page. closes #16

--- animal-magnetism-web/src/main/java/com/spring5/animalmagnetismpetclinic/controllers/VetController.java
+++ animal-magnetism-web/src/main/java/com/spring5/animalmagnetismpetclinic/controllers/VetController.java
@@ -1,13 +1,23 @@
 package com.spring5.animalmagnetismpetclinic.controllers;
 
+import com.spring5.animalmagnetismpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"","vets/index.html","vets","vets/index"})
-    public String listVets(){
+    public String listVets(Model model){
+
+        model.addAttribute("vets", vetService.findAll());
 
         return "vets/vetIndex";
     }
